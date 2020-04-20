3ca2cc59e3c585e845d750acc8bef034fe0a283d
JolinQChen <qiaolingchen1@gmail.com>
2020-04-19 23:03:25 -0400

    cql

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -3,14 +3,27 @@
 const app = getApp()
 
 Page({
-    temporal(){
-        wx.navigateTo({
-            url: "/pages/temporal/temporal"
-        })
-    },
-    spatial() {
-        wx.navigateTo({
-            url: "/pages/spatial/spatial"
-        })
-    }
+
+  temporal() {
+    wx.navigateTo({
+      url: "/pages/temporal/temporal"
+    })
+    wx.showLoading({
+      title: 'Loading...',
+    })
+    setTimeout(function () {
+      wx.hideLoading()
+    }, 1000)
+  },
+  spatial() {
+    wx.navigateTo({
+      url: "/pages/spatial/spatial"
+    })
+    wx.showLoading({
+      title: 'Loading...',
+    })
+    setTimeout(function () {
+      wx.hideLoading()
+    }, 1000)
+  }
 })
--- miniprogram/pages/temporal/temporal.js
+++ miniprogram/pages/temporal/temporal.js
@@ -1,93 +1,191 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+var dateTimePicker = require('../../Picker/dateTimePicker.js');
 
 let msg = ''
 let openid = ''
 let date = ''
 Page({
-    getMsg(e) {
-        console.log(e.detail.value)
-        msg = e.detail.value
-        this.setData({
-            msg: e.detail.value
-        })
-    },
-    // bind date change
-    bindDateChange(e) {
-        console.log(e.detail.value)
-        date = e.detail.value
-        this.setData({
-            date: e.detail.value
-        })
-    },
-    // save msg to db
-    saveMsg() {
-        let that = this
-        // authorize
-        wx.requestSubscribeMessage({
-            tmplIds: ["aDFqqiWr-YM6VBW4L6O9b-_7gvtdacntQMO0FTxdLCw"],
-            success(res) {
-                console.log("authorize success", res)
-                // get openid
-                wx.cloud.callFunction({
-                    name: "getopenId"
-                }).then(res => {
-                    // console.log("get openid success", res['result']['openid'])
-                    that.setData({
-                        _openid: res['result']['openid']
-                    })
-                    // console.log(that.data._openid)
-                    openid = res['result']['openid']
-                    // save msg
-                    wx.cloud.callFunction({
-                        name: 'saveMsg',
-                        data: {
-                            openid: that.data._openid,
-                            date: that.data.date,
-                            msg: that.data.msg
-                        }
-                    }).then(res => {
-                        console.log("save msg success", res)
-                    }).catch(err => {
-                        console.log("save msg fail", err)
-                    })
-                }).catch(err => {
-                    console.log("get openid fail", err)
-                })
-            },
-            fail(err) {
-                console.log("authorize fail", err)
-                // return err
-            }
-        })
-    },
-    // send subscription msg to a single user
-    // sendMsgSingleUser() {
-    //     if (msg == null || msg == '') {
-    //         wx.showToast({
-    //             icon: 'none',
-    //             title: 'please enter msg'
-    //         })
-    //     }
-    //     else {
-    //         this.send('o-gen5CF_b1vozM6BanyCcvX0Yo8', msg)
-    //     }
-    // },
+  // get user openid
+  // getOpenId() {
+  //     wx.cloud.callFunction({
+  //         name: "getopenId"
+  //     }).then(res => {
+  //         // console.log("get openid success", res['result']['openid'])
+  //         this.setData({
+  //             _openid: res['result']['openid']
+  //         })
+  //     }).catch(err => {
+  //         console.log("get openid fail", err)
+  //     })
+  // },
+  // authorize
+  // async authorize() {
+  //     wx.requestSubscribeMessage({
+  //         tmplIds: ["aDFqqiWr-YM6VBW4L6O9b-_7gvtdacntQMO0FTxdLCw"],
+  //         success(res) {
+  //             console.log("authorize success", res)
+  //             return res
+  //         },
+  //         fail(err) {
+  //             console.log("authorize fail", err)
+  //             return err
+  //         }
+  //     })
+  // },
 
-    // send msg
-    // send(openid, msg) {
-    //     wx.cloud.callFunction({
-    //         name: 'sendMsg',
-    //         data: {
-    //             openid: openid,
-    //             msg: msg
-    //         }
-    //     }).then(res => {
-    //         console.log("send msg to single user success", res)
-    //     }).catch(err => {
-    //         console.log("send msg to single user fail", err)
-    //     })
-    // }
+  // ================= cql =================
+  data: {
+    date: '2018-10-01',
+    time: '12:00',
+    dateTimeArray: null,
+    dateTime: null,
+    dateTimeArray1: null,
+    dateTime1: null,
+    startYear: 2000,
+    endYear: 2050,
+
+    "value": "",   // 文本的内容
+    "placeholder": "Enter Your Message Here",
+    "maxlength": -1,  // 最大输入长度，设置为 -1 的时候不限制最大长度
+    "focus": true,
+    "auto-height": true,  // 是否自动增高，设置auto-height时，style.height不生效
+    "adjust-position": true, // 键盘弹起时，是否自动上推页面
+
+  },
+  onLoad() {
+    // 获取完整的年月日 时分秒，以及默认显示的数组
+    var obj = dateTimePicker.dateTimePicker(this.data.startYear, this.data.endYear);
+    var obj1 = dateTimePicker.dateTimePicker(this.data.startYear, this.data.endYear);
+    // 精确到分的处理，将数组的秒去掉
+    var lastArray = obj1.dateTimeArray.pop();
+    var lastTime = obj1.dateTime.pop();
+
+    this.setData({
+      dateTime: obj.dateTime,
+      dateTimeArray: obj.dateTimeArray,
+      dateTimeArray1: obj1.dateTimeArray,
+      dateTime1: obj1.dateTime
+    });
+  },
+  changeDate(e) {
+    this.setData({ date: e.detail.value });
+  },
+  changeTime(e) {
+    this.setData({ time: e.detail.value });
+  },
+  changeDateTime(e) {
+    this.setData({ dateTime: e.detail.value });
+  },
+  changeDateTime1(e) {
+    this.setData({ dateTime1: e.detail.value });
+  },
+  changeDateTimeColumn(e) {
+    var arr = this.data.dateTime, dateArr = this.data.dateTimeArray;
+
+    arr[e.detail.column] = e.detail.value;
+    dateArr[2] = dateTimePicker.getMonthDay(dateArr[0][arr[0]], dateArr[1][arr[1]]);
+
+    this.setData({
+      dateTimeArray: dateArr,
+      dateTime: arr
+    });
+  },
+  changeDateTimeColumn1(e) {
+    var arr = this.data.dateTime1, dateArr = this.data.dateTimeArray1;
+
+    arr[e.detail.column] = e.detail.value;
+    dateArr[2] = dateTimePicker.getMonthDay(dateArr[0][arr[0]], dateArr[1][arr[1]]);
+
+    this.setData({
+      dateTimeArray1: dateArr,
+      dateTime1: arr
+    });
+  },
+  // ================= cql =================
+  getMsg(e) {
+    console.log(e.detail.value)
+    msg = e.detail.value
+    this.setData({
+      msg: e.detail.value
+    })
+  },
+
+  // 我把msg用多行输入储存了
+
+  // bind date change
+  bindDateChange(e) {
+    console.log(e.detail.value)
+    date = e.detail.value
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  // save msg to db
+  async saveMsg() {
+    // authorize
+    wx.requestSubscribeMessage({
+      tmplIds: ["aDFqqiWr-YM6VBW4L6O9b-_7gvtdacntQMO0FTxdLCw"],
+      success(res) {
+        console.log("authorize success", res)
+        // get openid
+        wx.cloud.callFunction({
+          name: "getopenId"
+        }).then(res => {
+          // console.log("get openid success", res['result']['openid'])
+          // this.setData({
+          //     _openid: res['result']['openid']
+          // })
+          openid = res['result']['openid']
+          // save msg
+          wx.cloud.callFunction({
+            name: 'saveMsg',
+            data: {
+              openid: openid,
+              date: date,
+              msg: msg
+            }
+          }).then(res => {
+            console.log("save msg success", res)
+          }).catch(err => {
+            console.log("save msg fail", err)
+          })
+        }).catch(err => {
+          console.log("get openid fail", err)
+        })
+      },
+      fail(err) {
+        console.log("authorize fail", err)
+        // return err
+      }
+    })
+  },
+  // send subscription msg to a single user
+  // sendMsgSingleUser() {
+  //     if (msg == null || msg == '') {
+  //         wx.showToast({
+  //             icon: 'none',
+  //             title: 'please enter msg'
+  //         })
+  //     }
+  //     else {
+  //         this.send('o-gen5CF_b1vozM6BanyCcvX0Yo8', msg)
+  //     }
+  // },
+
+  // send msg
+  // send(openid, msg) {
+  //     wx.cloud.callFunction({
+  //         name: 'sendMsg',
+  //         data: {
+  //             openid: openid,
+  //             msg: msg
+  //         }
+  //     }).then(res => {
+  //         console.log("send msg to single user success", res)
+  //     }).catch(err => {
+  //         console.log("send msg to single user fail", err)
+  //     })
+  // }
 })
